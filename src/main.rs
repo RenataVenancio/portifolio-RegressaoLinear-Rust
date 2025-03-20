d93334674f16dde6b6d96a1853a06747113e1416
RenataVenancio <renatavenancioalves12@gmail.com>
2025-03-20 17:37:15 -0300

    Commit Inicial do projeto

---
+++ src/main.rs
@@ -0,0 +1,6 @@
+mod regressao;
+
+fn main() {
+    // println!("Projeto de Regressão Linear em Rust!");
+    regressao::calcular_regressao();
+}
---
+++ src/regressao/mod.rs
@@ -0,0 +1,3 @@
+pub fn calcular_regressao() {
+    println!("Módulo de regressão linear carregado!");
+}
